a00c90ff46de98575919f37490642272897ded8f
Joseph Capper <60422562+JosephCapper@users.noreply.github.com>
2022-04-28 18:59:01 -0400

    Starting

---
+++ CatandMouse.java
@@ -0,0 +1,25 @@
+
+/**
+ * You will be given a string (x) featuring a cat 'C', a dog 'D' and a mouse 'm'. 
+ * The rest of the string will be made up of '.' (periods)
+ * 
+ * You need to find out if the cat can catch the mouse from it's current position.
+ * The cat can jump (j) characters.
+ * 
+ * Also, the cat cannot jump over the dog.
+ *
+ * @author Joseph Capper
+ * @version 4/28/2022
+ */
+public class CatandMouse
+{
+    public static void main (String args[]){
+        
+    }
+    public String isCaught(String x, int j) {
+        
+
+
+        return "";
+    }
+}
